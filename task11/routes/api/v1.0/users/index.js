fb906a829cc3e285f1ae9c95d83f4f8e0475a7eb
andrivash <andr.ivas12@gmail.com>
2017-05-12 00:27:01 +0300

    add user crud

--- task11/routes/api/v1.0/users/index.js
+++ task11/routes/api/v1.0/users/index.js
@@ -19,16 +19,69 @@ app.get('/', (req, res) => {
     });
 });
 
-app.post('/', (req, res) => {
-    const mongoose = require('../../../../libs/mongoose');
-    mongoose.connection.once('open', () => {
-        let user = new User({username: 'Vasya'});
-        user.save(function (err, user) {
-            if (err) res.send(new HttpError(500, err));
-            res.send(new HttpError(200, 'user saved'));
-            mongoose.disconnect();
+app.get('/:id', (req, res, next) => { //get /{userId}
+    if (req.params.id ) {
+        const mongoose = require('../../../../libs/mongoose');
+        mongoose.connection.once('open', () => {
+            User.findById(req.params.id, (err, user) =>{
+                if (err) res.send(new HttpError(200, 'data not found'));
+                res.send(user);
+                mongoose.disconnect();
+            });
         });
-
-    });
+    } else {
+        res.send(new HttpError(400, 'wronq query'));
+    }
 });
+
+app.post('/', (req, res) => { //post  ?name=
+    if (req.body.name) {
+        const mongoose = require('../../../../libs/mongoose');
+        mongoose.connection.once('open', () => {
+            let user = new User({username: req.body.name});
+            user.save(function (err, user) {
+                if (err) res.send(new HttpError(500, err));
+                res.send(new HttpError(200, 'user saved'));
+                mongoose.disconnect();
+            });
+        });
+    } else {
+        res.send(new HttpError(400, 'wronq query'));
+    }
+});
+
+app.patch('/:id', (req, res, next) => { //patch /{userId} ?name=
+    if (req.params.id && req.query.name) {
+        const mongoose = require('../../../../libs/mongoose');
+        mongoose.connection.once('open', () => {
+            User.findById(req.params.id, (err, user) =>{
+                if (err) res.send(new HttpError(200, 'data not found'));
+                user.username = req.query.name || user.username;
+                user.save(function (err, user) {
+                    if (err) res.send(new HttpError(500, err));
+                    res.send(new HttpError(200, 'user saved'));
+                    mongoose.disconnect();
+                });
+            });
+        });
+    } else {
+        res.send(new HttpError(400, 'wronq query'));
+    }
+});
+
+app.delete('/:id', (req, res) => {
+    if (req.params.id ) {
+        const mongoose = require('../../../../libs/mongoose');
+        mongoose.connection.once('open', () => {
+            User.findByIdAndRemove(req.params.id, (err, user) =>{
+                if (err) res.send(new HttpError(200, 'data not found'));
+                res.send(new HttpError(200, `user ${req.params.id} deleted`));
+                mongoose.disconnect();
+            });
+        });
+    } else {
+        res.send(new HttpError(400, 'wronq query'));
+    }
+});
+
 
